9d7db1b56eff492110c7ee6c965eeb650154ac85
Ruhan Asif <theruhaanasif@gmail.com>
2021-09-18 20:38:00 +0530

    first comment

---
+++ constructingArrays.js
@@ -0,0 +1,43 @@
+class newArray {
+  constructor(){
+    this.length = 0;
+    this.data = {};
+  }
+
+  get(index){
+    return this.data[index]
+  }
+
+  push(item){
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+
+  pop(){
+    const lastItem = this.data[this.length-1];
+    delete this.data[this.length-1];
+    this.length--;
+    return lastItem;
+  }
+
+  delete(index){
+    const item = this.data[index];
+    this.shiftItems(index);
+    return item;
+
+  }
+  shiftItems(index){
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i+1];
+      delete this.data[this.length - 1];
+      this.length--;
+    }
+  }
+}
+
+const playWithArray = new newArray();
+playWithArray.push('strawberry');
+playWithArray.push('parrot');
+playWithArray.delete(0);
+console.log(playWithArray);
